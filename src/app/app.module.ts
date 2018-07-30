ebc01454183fb06c6423b3f0856d29be9edb3468
Nagatoh <humberto.nagatog@gmail.com>
2018-07-30 17:16:18 -0300

    Tela de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+//Firebase config
+import { firebaseConfig } from '../configs/firebase';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 //pages
 import { LoginPageModule } from '../pages/login/login.module';
 
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @NgModule({
   declarations: [
@@ -16,6 +22,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     IonicModule.forRoot(MyApp),
     LoginPageModule,
   ],
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { trigger, style, animate, transition } from '@angular/animations';
 /**
  * Generated class for the LoginPage page.
  *
@@ -12,6 +12,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
+  animations: [
+    trigger(
+      'login', [
+        transition(':enter', [
+          style({
+            opacity: 0
+          }),
+          animate("1s ease-in-out", style({
+            opacity: 1
+          }))
+        ]),
+        transition(':leave', [
+          style({
+            opacity: 0
+          })
+        ])
+      ],
+    ),
+    trigger(
+      'registro', [
+        transition(':enter', [
+          style({
+            opacity: 0
+          }),
+          animate("1s ease-in-out", style({
+            opacity: 1
+          }))
+        ]),
+        transition(':leave', [
+          style({
+            opacity: 0
+          })
+        ])
+      ],
+    ),
+  ]
+
 })
 export class LoginPage {
   login = true;
@@ -23,7 +60,16 @@ export class LoginPage {
     console.log('ionViewDidLoad LoginPage');
   }
 
-  exibirRegistrar(){
-    
+  //exibir form de registro
+  exibirRegistrar() {
+    this.login = false;
+    this.register = true;
   }
+
+  //exibir form de login
+  exibirLogin() {
+    this.login = true;
+    this.register = false;
+  }
+
 }
