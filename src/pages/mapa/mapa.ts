eed404fc88f5bfa85f6c40640ca019371afb61e2
Nagatoh <humberto.nagatog@gmail.com>
2018-08-08 16:50:59 -0300

    config google maps

--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -1,14 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation';
 
-/**
- * Generated class for the MapaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 declare var google;
 
@@ -21,8 +15,11 @@ declare var google;
   ]
 })
 export class MapaPage {
-  options: GeolocationOptions;
-  currentPos: Geoposition;
+  public options: GeolocationOptions;
+  public currentPos: Geoposition;
+  @ViewChild('map') mapElement: ElementRef;
+  public map: any;
+  public places: Array<any>;
 
   constructor(
     public navCtrl: NavController,
@@ -42,16 +39,90 @@ export class MapaPage {
 
   getUserPosition() {
     this.options = {
-      enableHighAccuracy: true
+      enableHighAccuracy: false
     };
-
     this.geolocation.getCurrentPosition(this.options).then((pos: Geoposition) => {
 
       this.currentPos = pos;
+
       console.log(pos);
+      this.addMap(pos.coords.latitude, pos.coords.longitude);
 
     }, (err: PositionError) => {
       console.log("error : " + err.message);
+      ;
+    })
+  }
+
+  addMap(lat, long) {
+
+    let latLng = new google.maps.LatLng(lat, long);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    
+        this.getRestaurants(latLng).then((results: Array<any>) => {
+          this.places = results;
+          console.log('this.places', this.places);
+          for (let i = 0; i < results.length; i++) {
+            this.createMarker(results[i]);
+          }
+        }, (status) => console.log(status));
+    
+    this.addMarker();
+
+  }
+
+  addMarker() {
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<p>This is your current position !</p>";
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+  
+    getRestaurants(latLng) {
+      var service = new google.maps.places.PlacesService(this.map);
+      console.log('servicemap', service);
+      let request = {
+        location: latLng,
+        radius: 8047,
+        types: ["restaurant"]
+      };
+      return new Promise((resolve, reject) => {
+        service.nearbySearch(request, function (results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            resolve(results);
+          } else {
+            reject(status);
+          }
+  
+        });
+      });
+    }
+    
+
+  createMarker(place) {
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: place.geometry.location
     });
   }
 
